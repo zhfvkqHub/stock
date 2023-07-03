e3088eac05281479fbea0ccfb83127f41393465a
김다연 <hamoey@naver.com>
2023-07-03 23:29:59 +0900

    🎨 synchronized 사용하여 동시성 이슈 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +12,8 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-    public void decrease(Long id, Long quantity) {
-
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         // get stock
         Stock stock = stockRepository.findById(id)
                 .orElseThrow();
@@ -20,7 +21,6 @@ public class StockService {
         stock.decrease(quantity);
         // 저장
         stockRepository.saveAndFlush(stock);
-
     }
 
 }
--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -4,11 +4,16 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StockServiceTest {
@@ -40,6 +45,29 @@ class StockServiceTest {
                 .orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    @DisplayName("동시에 요청이 100개가 들어오는 테스트")
+    void 동시_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L, stock.getQuantity());
     }
 
 }
